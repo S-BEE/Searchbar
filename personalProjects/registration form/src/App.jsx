d8d3d8189ec641f5c082f1eca632d160b55b2edc
S-BEE <bshukurah96@gmail>
2023-08-24 15:22:46 +0100

    the post submission message has a bug

---
+++ personalProjects/registration form/src/App.jsx
@@ -0,0 +1,13 @@
+import Form from "./form"
+
+function App() {
+  
+
+  return (
+    <>
+     <Form/>
+    </>
+  )
+}
+
+export default App
---
+++ personalProjects/registration form/src/form.jsx
@@ -0,0 +1,57 @@
+import './form.css'
+import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+const Form = ()=>{
+    const [values, setValues] = useState({
+        firstName:'',
+        lastName:'',
+        email:'',
+    })
+    const [submitted, setSubmission] = useState(false)
+    const [valid, setValid] = useState(true)
+    
+
+    const handleFirstName = (e)=>{
+        setValues({...values, firstName:e.target.value})
+    }
+    const handleLastName = (e)=>{
+        setValues({...values, lastName:e.target.value})
+    }
+    const handleEmail = (e)=>{
+        setValues({...values, email:e.target.value})
+    }
+    const handleSubmit = (e)=>{
+       e.preventDefault();
+       if(values.firstName && values.lastName && values.email){
+
+       }
+       setSubmission(true);
+    }
+
+    return(
+        <>
+        <div className="" >
+            <div className="container">
+                <form action="" onSubmit={handleSubmit} >
+                   {submitted ? <div className="">Success! Thank you for registering</div> : null } 
+                    <input type="text" placeholder='First Name' name='firstName' value={values.firstName} onChange={handleFirstName} />
+                    { submitted && !values.firstName? <span>please, enter a first name!</span> : null }
+                    <input type="text" placeholder='Last Name' name='lastName' value={values.lastName} onChange={handleLastName} />
+                    {submitted && !values.lastName ? <span>please, enter a last name!</span> : null}
+                    <input type="text" placeholder='Email' id='email' value={values.email} onChange={handleEmail} />
+                    {submitted && !values.email.includes('@') ? <span>please, enter a valid email!</span> : null}
+                    <button type='submit'  className='btn' >Register</button>
+            
+
+                </form>
+            </div>
+        </div>
+        
+        </>
+
+    )
+}
+
+export default Form
